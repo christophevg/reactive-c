85dd6e0410ae9ade9e6ce813692dca9ecb391838
Christophe VG <contact@christophe.vg>
2015-04-02 09:45:29 +0200

    added debug_printf

--- src/reactive-c/debug.h
+++ src/reactive-c/debug.h
@@ -5,8 +5,10 @@
 #ifndef NDEBUG
 void _debug_level(char*, observable_t, int);
 #define _debug(t,o) _debug_level(t,o,0)
+#define _debug_printf(s,...) printf(s,__VA_ARGS__)
 #else
 #define _debug(t,o) ((void)0)
+#define _debug_printf(s,...) ((void)0)
 #endif
 
 #endif
