b47f72ff037025f9e32d4fb0fed27036e1eee288
Christophe VG <contact@christophe.vg>
2015-06-24 13:50:40 +0200

    added lift1

--- src/reactive-c/lift.h
+++ src/reactive-c/lift.h
@@ -7,6 +7,12 @@
     *(type*)(ob->observer) = fun((*(type*)(ob->observeds[0])), (*(type*)(ob->observeds[1]))); \
   }
 
+// demo for lifting through macro-expansion
+#define lift1(type, fun) \
+  void __lifted_##fun(observation_t ob) { \
+    *(type*)(ob->observer) = fun((*(type*)(ob->observeds[0]))); \
+  }
+
 // demo for lifting a folding function
 #define lift_fold1(type, fun) \
   void __lifted_##fun(observation_t ob) { \
